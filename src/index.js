308ccbc11fdc8c43b48a03da3403359c612cdaad
Lauri Pesonen <lauri.pesonen@iki.fi>
2022-06-20 10:09:46 +0100

    remove service worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactGA from "react-ga";
 import { QueryParamProvider } from "use-query-params";
 
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import {unregister} from "./registerServiceWorker";
 
 const gaId = process.env.REACT_APP_GA_ID; // "UA-168680303-1";
 if (gaId) {
@@ -19,4 +19,4 @@ root.render(
     <App isGaEnabled={!!gaId} />
   </QueryParamProvider>
 );
-registerServiceWorker();
+unregister();
